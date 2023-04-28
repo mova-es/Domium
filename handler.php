0856628e54e0d400209e3d3d673a547160518e00
Elvira Magasumova <mova.es94@gmail.com>
2023-04-28 23:27:11 +0500

    debug

--- handler.php
+++ handler.php
@@ -12,18 +12,18 @@ $phone = urldecode($phone);
 $username = trim($username);
 $phone = trim($phone);
 
-// echo $username;
-// echo "<br>";
-// echo $phone;
+echo $username;
+echo "<br>";
+echo $phone;
 
 // <!-- mail("на какой адрес отправить", "тема письма", "Сообщение (тело письма)","From: с какого email отправляется письмо \r\n"); -->
 
-if (mail("example@mail.ru", "Консультация с сайта", "Имя:".$username.". Телефон: ".$phone ,"From: example2@mail.ru \r\n"))
- {
-    echo "сообщение успешно отправлено";
-} else {
-    echo "при отправке сообщения возникли ошибки";
-}
+// if (mail("example@mail.ru", "Консультация с сайта", "Имя:".$username.". Телефон: ".$phone ,"From: example2@mail.ru \r\n"))
+//  {
+//     echo "сообщение успешно отправлено";
+// } else {
+//     echo "при отправке сообщения возникли ошибки";
+// }
 
 ?>
 
